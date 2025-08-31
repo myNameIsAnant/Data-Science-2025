18252aae8120089a36bec2b5492079e331b933d0
Anant <anant2011puri@gmail.com>
2025-08-31 17:58:22 +0530

    GuessGame Refactor

--- 02. Python/GuessGame.py
+++ 02. Python/GuessGame.py
@@ -1,7 +1,7 @@
 import random
 
 chosennos = random.randint(1,101)
-# print(chosennos)
+print(chosennos)
 
 """
 def usrpick(start,end):
@@ -10,17 +10,21 @@ def usrpick(start,end):
 
 def guessGame():
     usrchoice = []
+    low = 1
+    high = 100
     for i in range(1,6):
-        userchoice = int(input("Choose a number b/w 1 to 100: "))
+        userchoice = int(input(f"Attempt {i} of 5 - Choose a number b/w {low} to {high}: "))
         if userchoice == chosennos:
             return f"Great Your Chosen Number {userchoice} matches with Computer {chosennos}. You Won!!!"
         elif userchoice < chosennos:
             if i < 5:
                 print(f"Attempt {i} of 5 failed. Hint: Try Higher Number")
+                low = userchoice + 1
             usrchoice.append(userchoice)
         else:
             if i < 5:
                 print(f"Attempt {i} of 5 failed. Hint: Try Lower Number")
+                high = userchoice - 1
             usrchoice.append(userchoice)
     
     return f"You Lost!!! Your weren't able to guess the Chosen Number {chosennos}. You guessed {usrchoice}. Try Again!!!"
